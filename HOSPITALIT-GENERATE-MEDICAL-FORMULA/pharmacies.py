26c541b4951e3fb8b105934409b22f4977e5c99d
Alvaro Garzón <eduardo831_@hotmail.com>
2023-09-17 19:41:59 -0500

    refactor: Organized the order of the lambdas

--- HOSPITALIT-GENERATE-MEDICAL-FORMULA/pharmacies.py
+++
@@ -1,5 +0,0 @@
-from enum import Enum
-class Pharmacies(Enum):
-  SALUDPLUS = 'https://v3lrl256vt4fnktuqflj5ff2py0jmmxk.lambda-url.us-east-1.on.aws/'
-  VITALCARE = 'https://lln4lnsyqb.execute-api.us-east-1.amazonaws.com/prod/vitalcare'
-  BIENESTARTOTAL = 'https://lln4lnsyqb.execute-api.us-east-1.amazonaws.com/prod/bienestartotal'
--- HOSPITALIT-GENERATE-MEDICAL-FORMULA/utils/convert_medicines.py
+++
@@ -1,9 +0,0 @@
-def convert_medicines(medicines):
-    list_with_medicines_html = []
-    for item in medicines:
-        each_one = """<li><strong>Medicamento:</strong> {}</li>
-        <li><strong>Dosis:</strong> {}</li>
-        <li><strong>Instrucciones de Uso:</strong> {}.</li>""".format(item['medicine'], item['dose'], item['instructions'])
-        list_with_medicines_html.append(each_one)
-        return str(list_with_medicines_html).replace("[", "").replace(
-        "\\n", '<br>').replace("'", "").replace("]", "").replace("..", ".")
