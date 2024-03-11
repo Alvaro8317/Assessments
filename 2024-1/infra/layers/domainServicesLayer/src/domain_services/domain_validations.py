85445eb02b77b73e034e30a85dd44976f5f9a651
Alvaro Garzón <alvaro8317@SOLID-AG>
2024-03-11 12:10:05 -0500

    feat: Finishied feature of validate all products

--- 2024-1/infra/layers/domainServicesLayer/src/domain_services/domain_validations.py
+++ 2024-1/infra/layers/domainServicesLayer/src/domain_services/domain_validations.py
@@ -3,6 +3,14 @@ from ..helpers import DataValidators
 
 
 class DomainValidations:
+    validation_result_of_saving_accounts: bool = False
+    validation_result_of_saving_accounts: bool = False
+    validation_result_of_debit_card: bool = False
+    validation_result_of_credit_card: bool = False
+    validation_result_of_insurance: bool = False
+    validation_result_of_investments: bool = False
+    validation_result_of_covered_cards: Union[int, float] = 0
+
     def __init__(self) -> None:
         pass
 
@@ -63,4 +71,40 @@ class DomainValidations:
                 main_card_balance=main_card_balance, percentage_to_apply=5
             )
         return False
- 
+
+    @classmethod
+    def validate_all_products(cls, user_data: dict) -> dict:
+        country_user = user_data["country"]
+        age_user = user_data["age"]
+        incomes_user = user_data["incomes"]
+        cls.validation_result_of_saving_accounts = (
+            DomainValidations.validate_saving_accounts(
+                country=country_user, age=age_user
+            )
+        )
+        cls.validation_result_of_debit_card = DomainValidations.validate_debit_card(
+            country=country_user, age=age_user, incomes=incomes_user
+        )
+        cls.validation_result_of_credit_card = DomainValidations.validate_credit_card(
+            country=country_user, age=age_user, incomes=incomes_user
+        )
+        cls.validation_result_of_insurance = DomainValidations.validate_insurance(
+            age=age_user, incomes=incomes_user
+        )
+        cls.validation_result_of_investments = DomainValidations.validate_investments(
+            age=age_user, incomes=incomes_user
+        )
+        cls.validation_result_of_covered_cards = (
+            DomainValidations.validate_covered_cards(
+                age=age_user, main_card_balance=incomes_user
+            )
+        )
+        return {
+            "validation_result_of_saving_accounts": cls.validation_result_of_saving_accounts,
+            "validation_result_of_saving_accounts": cls.validation_result_of_saving_accounts,
+            "validation_result_of_debit_card": cls.validation_result_of_debit_card,
+            "validation_result_of_credit_card": cls.validation_result_of_credit_card,
+            "validation_result_of_insurance": cls.validation_result_of_insurance,
+            "validation_result_of_investments": cls.validation_result_of_investments,
+            "validation_result_of_covered_cards": cls.validation_result_of_covered_cards,
+        }
