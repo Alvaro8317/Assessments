d75a30c941f53e587cfdc24987b3279556f8a879
Alvaro Garzón <alvaro8317@SOLID-AG>
2024-03-11 13:20:46 -0500

    feat: Add support to IA

---
+++ 2024-1/infra/ia/src/app.py
@@ -0,0 +1,32 @@
+from openai import OpenAI
+from aws_lambda_powertools.utilities.typing import LambdaContext
+from aws_lambda_powertools.logging import correlation_paths
+from aws_lambda_powertools import Logger
+from aws_lambda_powertools import Tracer
+from aws_lambda_powertools import Metrics
+
+tracer = Tracer()
+logger = Logger()
+metrics = Metrics(namespace="Powertools")
+create_response = lambda message: {"comments_of_ia": message}
+client = OpenAI()
+
+
+@logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_REST)
+@tracer.capture_lambda_handler
+@metrics.log_metrics(capture_cold_start_metric=True)
+def lambda_handler(event: dict, context: LambdaContext) -> dict:
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": event["training"],
+            },
+            {
+                "role": "user",
+                "content": event["result_validation"],
+            },
+        ],
+    )
+    return create_response(completion.choices[0].message)
